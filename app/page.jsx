9604dbad7366b42b512cb20b8424fc4c6e77a9b8
Isaac-hx <saydimas78@gmail.com>
2024-06-18 13:35:08 +0700

    fix size container on mobile

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default async function Home(){
 
   return(
     <div >
-      <div className="w-1/2 rounded-lg space-y-8 bg-white mt-4 p-2 mx-auto shadow-lg">
+      <div className="md:w-1/2 w-4/5 rounded-lg space-y-8 bg-white mt-4 p-2 mx-auto shadow-lg">
         <Header/>
         <hr/>
         <Body/>
